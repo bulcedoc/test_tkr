bd83adae508319b41f96af8bda5f31cdbe06ed53
bulcedoc <blucedoc@gmail.com>
2022-10-30 04:06:10 +0530

    secound

--- Project/settings.py
+++ Project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-654!!lr(o=-9^&#p%h36h#sxo%u50ik$c7%#w!!yt)&y(_&a$y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
